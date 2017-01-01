e385887a1a17d14d8b29095ee2f5221d9246d444
Dan Kellett <bobbins3000@hotmail.co.uk>
2017-01-01 11:27:46 +0000

    Data read-in from GitHub & charting of data

--- code/1DataReadinExploration.R
+++ code/1DataReadinExploration.R
@@ -6,14 +6,46 @@
 # Author: Dan Kellett               #
 #####################################
 
-# Data has already been dowloaded from Kaggle to 'titanic/inputs'
+# Note this code draws heavily from the souptonuts repo from wehrley:
+# https://github.com/wehrley/wehrley.github.io/blob/master/SOUPTONUTS.md
+
+# Data has already been loaded from Kaggle to 'titanic/inputs' to my GitHub repo
+
+# Create a generic .csv read-in function
+readData <- function(pathName, fileName, columnTypes, missingTypes) {
+  read.csv( url( paste(pathName, fileName, sep="") ), 
+            colClasses=columnTypes,
+            na.strings=missingTypes )
+}
 
 # Set working directory
 setwd("C:/Users/Oaklett/Desktop/Dan/Data Science/Kaggle/Titanic/data")
 
 # Read-in data
-train <- read.csv('C:/Users/Oaklett/Desktop/Dan/Data Science/Kaggle/Titanic/data/inputs/train.csv', stringsAsFactors = F)
-test <- read.csv('C:/Users/Oaklett/Desktop/Dan/Data Science/Kaggle/Titanic/data/inputs/test.csv', stringsAsFactors = F)
+titanicPath <- "http://raw.github.com/bobbins3000/kaggleTitanic/master/data/inputs/"
+trainName <- "train.csv"
+testName <- "test.csv"
+missing <- c("NA", "")
+trainColumnTypes <- c('integer',   # PassengerId
+                        'factor',    # Survived 
+                        'factor',    # Pclass
+                        'character', # Name
+                        'factor',    # Sex
+                        'numeric',   # Age
+                        'integer',   # SibSp
+                        'integer',   # Parch
+                        'character', # Ticket
+                        'numeric',   # Fare
+                        'character', # Cabin
+                        'factor'     # Embarked
+)
+testColumnTypes <- trainColumnTypes[-2] 
 
-summary(train)
-str(train)
+trainRaw <- readData(titanicPath, trainName, 
+                      trainColumnTypes, missing)
+
+testRaw <- readData(titanicPath, testName, 
+                     testColumnTypes, missing)
+
+
+
